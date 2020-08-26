992b5854cb5a54360b3ce2f62de0a0da064d2801
A. A. Thiruvalluvan <a.a.thiruvalluvan@avanade.com>
2020-08-26 14:57:18 -0400

    testing

--- iothub_client/samples/iothub_ll_telemetry_sample/iothub_ll_telemetry_sample.c
+++ iothub_client/samples/iothub_ll_telemetry_sample/iothub_ll_telemetry_sample.c
@@ -188,7 +188,7 @@ int main(void)
             }
 
             IoTHubDeviceClient_LL_DoWork(device_ll_handle);
-            ThreadAPI_Sleep(910);
+            ThreadAPI_Sleep(993);
 
         } while (g_continueRunning);
 
