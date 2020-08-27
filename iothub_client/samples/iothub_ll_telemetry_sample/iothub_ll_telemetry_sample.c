36aad484ac76a5521521bd9c80441dc813195ea0
A. A. Thiruvalluvan <a.a.thiruvalluvan@avanade.com>
2020-08-27 14:33:02 -0400

    update

--- iothub_client/samples/iothub_ll_telemetry_sample/iothub_ll_telemetry_sample.c
+++ iothub_client/samples/iothub_ll_telemetry_sample/iothub_ll_telemetry_sample.c
@@ -49,7 +49,7 @@ and removing calls to _DoWork will yield the same results. */
 #endif // SAMPLE_HTTP
 
 /* Paste in the your iothub connection string  */
-static const char *connectionString = "HostName=iot-danadf-dev.azure-devices.net;DeviceId=clt17-v001-dev-00000001;SharedAccessKey=ho5qKYSNwtpNlLdLznAwHRvi+O4rqHtwsXw8nnapUY0=";
+static const char *connectionString = "HostName=iot-danadf-sb.azure-devices.net;DeviceId=clt17-v0001-000001;SharedAccessKey=TcECgUCuNCiqNBVnvW9JH3Q7i9tATI3mJIOBYV3Drwg=";
 #define MESSAGE_COUNT 100
 static bool g_continueRunning = true;
 static size_t g_message_count_send_confirmations = 0;
